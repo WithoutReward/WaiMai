ad723919602edeb2b81118fb05cba13ef135e0ca
WithoutReward <rorycao@foxmail.com>
2024-03-11 16:16:42 +0800

    启用禁用员工账号业务

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -111,4 +111,24 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         return new PageResult(total,records);
     }
+
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     */
+    public void startOrStop(Integer status, Long id) {
+        //将status和id包装进Employee对象
+//        法一：直接创建对象
+//        Employee employee = new Employee();
+//        employee.setStatus(status);
+//        employee.setId(id);
+//        法二：使用构建器创建对象
+        Employee employee = Employee.builder()
+                .status(status)
+                .id(id).build();
+
+        employeeMapper.update(employee);
+
+    }
 }
