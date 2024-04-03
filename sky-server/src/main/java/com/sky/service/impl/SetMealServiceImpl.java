b0c5a39c842604821421771b4201e34190ea30c8
WithoutReward <rorycao@foxmail.com>
2024-04-03 13:09:49 +0800

    用户端商品浏览相关开发

--- sky-server/src/main/java/com/sky/service/impl/SetMealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetMealServiceImpl.java
@@ -15,6 +15,7 @@ import com.sky.mapper.SetMealDishMapper;
 import com.sky.mapper.SetmealMapper;
 import com.sky.result.PageResult;
 import com.sky.service.SetMealService;
+import com.sky.vo.DishItemVO;
 import com.sky.vo.SetmealVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -147,5 +148,24 @@ public class SetMealServiceImpl implements SetMealService {
         Setmeal setmeal = setmealMapper.getById(id);
         setmeal.setStatus(Integer.valueOf(status));
         setmealMapper.update(setmeal);
+    }
+
+    /**
+     * 条件查询
+     * @param setmeal
+     * @return
+     */
+    public List<Setmeal> list(Setmeal setmeal) {
+        List<Setmeal> list = setmealMapper.list(setmeal);
+        return list;
+    }
+
+    /**
+     * 根据id查询菜品选项
+     * @param id
+     * @return
+     */
+    public List<DishItemVO> getDishItemById(Long id) {
+        return setmealMapper.getDishItemBySetmealId(id);
     }
 }
